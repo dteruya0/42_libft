36e8820bd3a3615a0cf959e3d0b180138d24e39f
Diego Teruya <diegoteruya@192.168.1.21>
2024-11-06 18:54:54 -0300

    ˜libft˜

---
+++ ft_lstadd_back_bonus.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstadd_back_bonus.c                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: diegoteruya <diegoteruya@student.42.fr>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/06 18:45:01 by diegoteruya       #+#    #+#             */
+/*   Updated: 2024/11/06 18:50:56 by diegoteruya      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstadd_back(t_list **lst, t_list *new)
+{
+	if (*lst == NULL)
+	{
+		ft_lstadd_front
+	}
+}
