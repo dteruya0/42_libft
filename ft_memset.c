3798ad82ffed058daed3b53266d1223a1880692c
Diego Teruya <dteruya@c1r8p1.42sp.org.br>
2024-11-08 08:25:01 -0300

    memset

--- ft_memset.c
+++ ft_memset.c
@@ -6,7 +6,7 @@
 /*   By: dteruya <dteruya@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/14 14:02:56 by dteruya           #+#    #+#             */
-/*   Updated: 2024/11/05 14:12:38 by dteruya          ###   ########.fr       */
+/*   Updated: 2024/11/08 08:22:59 by dteruya          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,12 @@
 
 void	*ft_memset(void *s, int c, size_t n)
 {
-	size_t			index;
-	unsigned char	*str;
+	size_t	index;
 
 	index = 0;
-	str = (unsigned char *) s;
 	while (index < n)
 	{
-		str[index] = (unsigned char)c;
+		((unsigned char *)s)[index] = (unsigned char)c;
 		index++;
 	}
 	return (s);
